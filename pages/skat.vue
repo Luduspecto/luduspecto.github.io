a62b3f249b1bdad7562b6abd2632ce0b2ad583d4
MaxKranabetter <kranabettermaximilian@gmail.com>
2025-03-13 20:03:49 +0100

    fix typo

--- pages/skat.vue
+++ pages/skat.vue
@@ -1,7 +1,7 @@
 <template>
     <LandingContainer>
 
-        <AppHero googel_url="ttps://play.google.com/store/apps/details?id=de.offlinelernen.skat" image_url="/img/skat_icon.png" apple_url="https://apps.apple.com/de/app/skat-offline-lernen/id6740433269">
+        <AppHero googel_url="https://play.google.com/store/apps/details?id=de.offlinelernen.skat" image_url="/img/skat_icon.png" apple_url="https://apps.apple.com/de/app/skat-offline-lernen/id6740433269">
             <template v-slot:title>Skat Offline Lernen</template>
             <template v-slot:desc>Offline, interaktiv und in deinem Tempo.</template>
         </AppHero>
